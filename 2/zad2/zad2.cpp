82feaa2d92fb173122cd1ed9763010bd77731230
dbrcina <darijo.brcina@fer.hr>
2020-04-08 20:04:27 +0200

    wip

--- 2/zad2/zad2.cpp
+++ 2/zad2/zad2.cpp
@@ -8,51 +8,56 @@ Iterator mymax(Iterator cur, Iterator last, Predicate pred) {
 	Iterator max = cur;
 	cur++;
 	while (cur != last) {
-		if (pred(cur, max) == 1) max = cur;
+		if (pred(*cur, *max) == 1) max = cur;
 		cur++;
 	}
 	return max;
 }
 
-int gt_int(const void *a, const void *b) {
-	return *(int*)a > *(int*)b ? 1 : 0;
+int gt_int(const int a, const int b) {
+	return a > b ? 1 : 0;
 }
-int gt_char(const void *c1, const void *c2) {
-	return *(char*)c1 > *(char*)c2 ? 1 : 0;
+int gt_char(const char c1, const char c2) {
+	return c1 > c2 ? 1 : 0;
 }
-int gt_str(const void *s1, const void *s2) {
-	return strcmp(*(char**)s1, *(char**)s2) > 0 ? 1 : 0;
+int gt_str(const char *s1, const char *s2) {
+	return strcmp(s1, s2) > 0 ? 1 : 0;
 }
 
 int main() {
 	int arr_int[] = {1, 3, 5, 7, 4, 6, 9, 2, 0};
-	const int *maxint = mymax(&arr_int[0], &arr_int[sizeof(arr_int)/sizeof(*arr_int)], gt_int);
-	std::cout <<*maxint <<"\n";
+	int n_arr_int = sizeof(arr_int) / sizeof(*arr_int);
+	std::vector<int> vec_int(arr_int, arr_int + n_arr_int);
+	std::set<int> set_int(vec_int.begin(), vec_int.end());
 
 	char arr_char[] = "Suncana strana ulice";
-	const char *maxchar = mymax(&arr_char[0], &arr_char[sizeof(arr_char)/sizeof(*arr_char)], gt_char);
-	std::cout <<*maxchar <<"\n";
+	int n_arr_char = sizeof(arr_char) / sizeof(*arr_char);
+	std::vector<char> vec_char(arr_char, arr_char + n_arr_char);
+	std::set<char> set_char(vec_char.begin(), vec_char.end());
 
 	const char *arr_str[] = {
 		"Gle", "malu", "vocku", "poslije", "kise",
    		"Puna", "je", "kapi", "pa", "ih", "njise"
 	};
-	const char **maxstring = mymax(&arr_str[0], &arr_str[sizeof(arr_str)/sizeof(*arr_str)], gt_str);
-	std::cout <<*maxstring <<"\n";
+	int n_arr_str = sizeof(arr_str) / sizeof(*arr_str);
+	std::vector<const char*> vec_str(arr_str, arr_str + n_arr_str);
+	std::set<const char*> set_str(vec_str.begin(), vec_str.end());
 
-	std::vector<int> vectorInts(arr_int, arr_int + sizeof(arr_int) / sizeof(int));
-	const int *maxintV = mymax(&vectorInts.front(), &vectorInts.back(), gt_int);
-	std::cout <<*maxintV <<"\n";
+	// ARRAYS
+	std::cout << "Arrays:\n";
+	std::cout << *mymax(&arr_int[0], &arr_int[n_arr_int], gt_int) << "\n";
+	std::cout << *mymax(&arr_char[0], &arr_char[n_arr_char], gt_char) << "\n";
+	std::cout << *mymax(&arr_str[0], &arr_str[n_arr_str], gt_str) <<"\n";
 
-	std::vector<char> vectorChars(arr_char, arr_char + sizeof(arr_char) / sizeof(char));
-	const char *maxcharV = mymax(&vectorChars.front(), &vectorChars.back(), gt_char);
-	std::cout <<*maxcharV <<"\n";
+	// VECTORS
+	std::cout << "\nVectors:\n";
+	std::cout << *mymax(vec_int.begin(), vec_int.end(), gt_int) << "\n";
+	std::cout << *mymax(vec_char.begin(), vec_char.end(), gt_char) << "\n";
+	std::cout << *mymax(vec_str.begin(), vec_str.end(), gt_str) << "\n";
 
-	std::vector<const char*> vectorStrings(arr_str, arr_str + sizeof(arr_str) / sizeof(char*));
-	const char **maxstringV = mymax(&vectorStrings.front(), &vectorStrings.back(), gt_str);
-	std::cout <<*maxstringV <<"\n";
-
-	std::set<int> setInts(arr_int, arr_int + sizeof(arr_int) / sizeof(int));
-	const int *maxintS = mymax(setInts.begin(), setInts.end(), gt_int);
-	std::cout <<*maxintS <<"\n";
+	// SETS
+	std::cout << "\nSets:\n";
+	std::cout << *mymax(set_int.begin(), set_int.end(), gt_int) << "\n";
+	std::cout << *mymax(set_char.begin(), set_char.end(), gt_char) << "\n";
+	std::cout << *mymax(set_str.begin(), set_str.end(), gt_str) << "\n";
 }
